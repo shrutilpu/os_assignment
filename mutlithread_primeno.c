43c42d8d875bf2a392bc61edd00c7ef2b7e85871
shrutilpu <49159599+shrutilpu@users.noreply.github.com>
2020-03-29 13:20:08 +0530

    prime_no.c
    checking prime or not

--- mutlithread_primeno.c
+++ mutlithread_primeno.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+#include<pthread.h>
+#include<unistd.h>
+void *thread1(void *p);
+bool isprime(int m)	
+{
+for(int i=2;i<=m/2;i++)
+	{
+    	if(m%i==0)
+	  {
+		return false;
+	  }
+	}	
+}
